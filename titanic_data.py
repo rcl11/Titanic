4f1301fc28c6f9129d73ea6ac104df6bf3ef947d
rcl11 <rebecca.lane@qmul.ac.uk>
2018-03-19 10:08:51 -0400

    Initial commit of my messings around

---
+++ titanic_data.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import tensorflow as tf
+
+TRAIN_PATH = "train_perfect.csv"
+#TEST_PATH = "test.csv"
+TEST_PATH = "train_perfect_2.csv"
+
+CSV_COLUMN_NAMES = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+SPECIES = ['Survived','Died']
+
+
+def load_data(y_name='Survived'):
+    """Returns the Titanic dataset as (train_x, train_y), (test_x, test_y)."""
+
+    train = pd.read_csv(TRAIN_PATH, names=CSV_COLUMN_NAMES, header=0)
+    train.fillna('', inplace=True)
+    train[train.isnull().any(axis=1)]
+    train_x, train_y = train, train.pop(y_name)
+
+    test = pd.read_csv(TEST_PATH, names=CSV_COLUMN_NAMES, header=0)
+    test.fillna('', inplace=True)
+    test_x, test_y = test, test.pop(y_name)
+
+    return (train_x, train_y), (test_x, test_y)
+
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+    # Return the dataset.
+    return dataset
+
+
+def eval_input_fn(features, labels, batch_size):
+    """An input function for evaluation or prediction"""
+    features=dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the dataset.
+    return dataset
+
+
+# The remainder of this file contains a simple example of a csv parser,
+#     implemented using a the `Dataset` class.
+
+# `tf.parse_csv` sets the types of the outputs to match the examples given in
+#     the `record_defaults` argument.
+CSV_TYPES = [[0.0], [0.0], [0.0], [0.0], [0]]
+
+def _parse_line(line):
+    # Decode the line into its fields
+    fields = tf.decode_csv(line, record_defaults=CSV_TYPES)
+
+    # Pack the result into a dictionary
+    features = dict(zip(CSV_COLUMN_NAMES, fields))
+
+    # Separate the label from the features
+    label = features.pop('Species')
+
+    return features, label
+
+
+def csv_input_fn(csv_path, batch_size):
+    # Create a dataset containing the text lines.
+    dataset = tf.data.TextLineDataset(csv_path).skip(1)
+
+    # Parse each line.
+    dataset = dataset.map(_parse_line)
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+    # Return the dataset.
+    return dataset
+    
